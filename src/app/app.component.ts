41c323f3d72d9f5514c2cfae46f1879a61689e98
Jasper Van Proeyen <trianglejuice@mac.com>
2018-11-17 10:50:01 +0100

    Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-gh-pages';
+  title = 'GitHub pages 404 hack tester';
 }
